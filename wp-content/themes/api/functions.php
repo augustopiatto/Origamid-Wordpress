50ce702d74abfdf2f0beca2917455334e7d62b57
Augusto <ampiatto@hotmail.com>
2023-06-20 08:47:02 -0300

    aula 5 de wordpress

--- wp-content/themes/api/functions.php
+++ wp-content/themes/api/functions.php
@@ -1,12 +1,19 @@
 <!-- Se quando entrar no localhost nao ver nada, é porque falta logar -->
 <!-- Entre no /wp-admin -->
-<?php 
 
+<?php
 $template_diretorio = get_template_directory();
 
 require_once($template_diretorio . "/custom-post-type/produto.php");
 require_once($template_diretorio . "/custom-post-type/transacao.php");
 
-require_once($template_diretorio . "/endpoints/usuario-post.php");
+require_once($template_diretorio . "/endpoints/usuario_post.php");
+require_once($template_diretorio . "/endpoints/usuario_get.php");
+
+function expire_token() {
+    return time() + (60 * 60 * 24);
+}
+
+add_action('jwt_auth_expire', 'expire_token');
 
 ?>
